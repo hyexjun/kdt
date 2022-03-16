27e7566026becee005cfd9c838fd62465b259d27
hyexjun <parkhye.chu@gmail.com>
2022-03-16 13:01:19 +0900

    js update

---
+++ 04_JS/0316/practice.js
@@ -0,0 +1,18 @@
+
+// 함수 선언
+
+function add(a, b) {
+    const sum = a + b;
+    return sum;
+}
+
+function doSomething(add) {
+    console.log(add);
+    const result = sum;
+    console.log(result);
+}
+
+
+// 함수 호출
+
+console.log(result);
