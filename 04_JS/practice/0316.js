b7df05d952b48cc0d6b7494b347759b187a5ac77
hyexjun <parkhye.chu@gmail.com>
2022-03-16 14:52:22 +0900

    js add

--- 04_JS/practice/0316.js
+++ 04_JS/practice/0316.js
@@ -1,3 +1,8 @@
+// doSomething(add);
+// 오.. 진짜 함수 선언(때 함수명 적은 애들)은.. 호이스팅이.. 되네.....
+// dodo(add);
+// 이건 함수 선언때는 노네임인데 const로 이름을 표현한 거라 호이스팅 안됨
+
 
 // 함수 선언
 
@@ -12,7 +17,14 @@ function doSomething(add) {
     console.log(result);
 }
 
+const dodo = function (add) {
+    console.log(add);
+    const result = add(5, 3);
+    console.log(result);
+}
+
 
 // 함수 호출
 
 doSomething(add);
+dodo(add);
