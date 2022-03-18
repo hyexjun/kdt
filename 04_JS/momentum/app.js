9d67d793b918c2f77d472e259b6da123b2bd00d4
hyexjun <97497201+hyexjun@users.noreply.github.com>
2022-03-18 15:46:19 +0900

    Update app.js

--- 04_JS/momentum/app.js
+++ 04_JS/momentum/app.js
@@ -3,19 +3,23 @@
 // const loginButton = loginForm.querySelector("button");
 
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+// const loginButton = document.querySelector("#login-form button");
 // 위 세 줄이랑 같은 두 줄
+const loginForm = document.querySelector("#login-form")
 
-
-function onLoginBtnClick() {
+function onLoginSubmit(event) {
     // console.log(`hello!! ${loginInput.value}`);
     // console.log("click!!!")
-    const username = loginInput.value;
-    if (username === "") {
-        alert("Please write your name");
-    } else if (username.length > 15) {
-        alert("your name is too long!");
-    }
+
+    event.preventDefault();
+    // const username = loginInput.value;
+    // console.log(username);
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+
+// loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener('submit', onLoginSubmit);
+// 일종의 조건문처럼 addEventListener(A, B);
+// A라는 조건에 걸릴 때, B를 실행하겠음 이기 때문에 B()라고 쓰지 않는다
+// B()는 걍 바로 함수 호출이니까..
