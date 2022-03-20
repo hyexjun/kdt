9761d2a9b9c0d115527f66d507b1f9ebcbbf6bed
hyexjun <parkhye.chu@gmail.com>
2022-03-20 18:19:32 +0900

    momentum update

--- 04_JS/momentum/app.js
+++ 04_JS/momentum/app.js
@@ -1,21 +1,23 @@
-// const loginForm = document.getElementById("login-form");
-// const loginInput = loginForm.querySelector("input");
-// const loginButton = loginForm.querySelector("button");
-
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
-// 위 세 줄이랑 같은 두 줄
+const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
+// 아래 class add 혹은 remove 시 "hidden"을 여러번 쓰니까 변수 지정했다는데
+// 그게 뭔 상관인지 몰겠음.......
 
-function onLoginBtnClick() {
-    // console.log(`hello!! ${loginInput.value}`);
-    // console.log("click!!!")
-    const username = loginInput.value;
-    if (username === "") {
-        alert("Please write your name");
-    } else if (username.length > 15) {
-        alert("your name is too long!");
-    }
+function onLoginSubmit(event) {
+
+    event.preventDefault();  // 막아주기..? 이거 토마토..
+    const username = loginInput.value;  // 인풋 입력값 변수 할당
+    localStorage.setItem("username", username);
+    // js용 미니DB라고 보면 되고 f12 앱쪽에서 볼수있음
+
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    // submit 제출 실행 시 hidden 클래스 추가로 form 숨겨버리기
+    greeting.innerText = `No sibal Keep going, ${username}!!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
